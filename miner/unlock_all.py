65283f5be7f9ec12d25ad65016c0efa91f74a740
Jeff MacLoue <jeff.macloue@automat-it.com>
2019-08-15 18:48:10 +0300

    Miner unlock_all: changed to newer database format

--- miner/unlock_all.py
+++ miner/unlock_all.py
@@ -7,16 +7,13 @@ from pprint import pprint,pformat
 dynamodb = botocore.session.get_session().create_client("dynamodb", region_name="us-east-1")
 SPACEMESH_DYNAMODB_TABLE = "initdata-testnet-us-east-1.spacemesh.io"
 
-for item in dynamodb.query(TableName=SPACEMESH_DYNAMODB_TABLE,
-                           IndexName="locked",
-                           KeyConditionExpression="locked = :true",
-                           ExpressionAttributeValues={":true": {"N": "1"}},
-                           ProjectionExpression="id",
-                           )["Items"]:
+for item in dynamodb.scan(TableName=SPACEMESH_DYNAMODB_TABLE,
+                          ProjectionExpression="id",
+                          )["Items"]:
     item_id = item["id"]["S"]
     print("Unlocking '{}'".format(item_id))
     pprint(dynamodb.update_item(TableName=SPACEMESH_DYNAMODB_TABLE,
                                 Key={"id": {"S": item_id}},
-                                UpdateExpression="SET locked = :false",
+                                UpdateExpression="SET locked = :false REMOVE locked_by",
                                 ExpressionAttributeValues={":false": {"N": "0"}},
                                 ))

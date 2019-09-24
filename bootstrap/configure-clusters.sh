e3efe3171b54ed4fe19b583d6b7f0a9126c941c9
Jeff MacLoue <jeff.macloue@automat-it.com>
2019-09-24 16:13:40 +0300

    Refactored to a single PoET instance

--- bootstrap/configure-clusters.sh
+++ bootstrap/configure-clusters.sh
@@ -2,13 +2,7 @@
 
 REGIONS="ap-northeast-2 eu-north-1 us-east-1 us-east-2 us-west-2"
 
-declare -A POET
-POET["ap-northeast-2"]="spacemesh-testnet-poet-grpc-lb-8f4a4fc1729801b2.elb.ap-northeast-2.amazonaws.com:50002"
-POET["eu-north-1"]="spacemesh-testnet-poet-grpc-lb-a390c50a80ea4435.elb.eu-north-1.amazonaws.com:50002"
-POET["us-east-1"]="spacemesh-testnet-poet-grpc-lb-6a3ace93a4d7c65d.elb.us-east-1.amazonaws.com:50002"
-POET["us-east-2"]="spacemesh-testnet-poet-grpc-lb-01b0da360834b7eb.elb.us-east-2.amazonaws.com:50002"
-POET["us-west-2"]="spacemesh-testnet-poet-grpc-lb-526f896bbc911d0e.elb.us-west-2.amazonaws.com:50002"
-
+POET_URL="spacemesh-testnet-poet-grpc-lb-949d0cde858743fb.elb.us-east-1.amazonaws.com:50002"
 COINBASE="0x1234"
 
 MANIFEST=$(mktemp)
@@ -26,7 +20,7 @@ data:
   initdata_dynamodb_table: testnet-initdata.${REGION}.spacemesh.io
   initdata_s3_bucket: testnet-initdata.${REGION}.spacemesh.io
   initdata_s3_prefix: ""
-  poet_url: ${POET[$REGION]}
+  poet_url: ${POET_URL}
   poet_nodeaddr: ""
   spacemesh_coinbase: "${COINBASE}"
 EOF

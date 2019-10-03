4842ddee43e1bea6bdbddaff8efa319e00166b01
Jeff MacLoue <jeff.macloue@automat-it.com>
2019-10-03 18:00:09 +0300

    Fixup for fluent-bit in mgmt

--- bootstrap/_config.inc.sh
+++ bootstrap/_config.inc.sh
@@ -3,4 +3,5 @@ REGIONS="ap-northeast-2 eu-north-1 us-east-1 us-east-2 us-west-2"
 declare -A POOLS
 POOLS[miner]="master miner"
 POOLS[initfactory]="master initfactory"
-POOLS[mgmt]="master logging poet"
+POOLS[mgmt]="master poet"
+#POOLS[mgmt]="master logging poet"

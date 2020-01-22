c971cfa89e1b7958e8b56d386ce4b1437d6ed63a
Jeff MacLoue <jeff.macloue@automat-it.com>
2020-01-22 22:40:52 +0200

    logging: Merge changes to prod

--- bootstrap/logging/efk/es_settings.sh
+++ bootstrap/logging/efk/es_settings.sh
@@ -19,55 +19,16 @@ $curl -XPUT $LOGS_ES_HOST"/_ingest/pipeline/ingest_timestamp_pipeline?pretty" -d
   ]
 }'
 
-echo "Creating index_delete lifecycle policy"
-$curl -XPUT $LOGS_ES_HOST"/_ilm/policy/index_delete?pretty" -d \
-'{
-  "policy": {
-    "phases": {
-      "hot": {
-        "min_age": "0ms",
-        "actions": {}
-      },
-      "warm": {
-        "min_age": "1d",
-        "actions": {"readonly":{}, "shrink": {"number_of_shards": "1"}}
-      },
-      "cold": {
-        "min_age": "2d",
-        "actions": {"freeze": {}}
-      },
-      "delete": {
-        "min_age": "7d",
-        "actions": {"delete": {}}
-      }
-    }
-  }
-}'
-
 echo "Creating kubernetes_cluster_template"
 $curl -XPUT $LOGS_ES_HOST"/_template/kubernetes_cluster_template?pretty" -d \
 '{
   "index_patterns": ["kubernetes_cluster-*"],
   "settings": {
     "index": {
-      "refresh_interval": "30s",
-      "number_of_shards": "3",
-      "number_of_replicas": "0",
       "default_pipeline": "ingest_timestamp_pipeline",
-      "translog": { "durability": "async" },
-      "lifecycle": {
-        "name": "index_delete",
-        "rollover_alias": ""
-      }
+      "number_of_replicas": "0"
     }
   }
-}'
-
-echo "Adjusting global settings"
-$curl -XPUT $LOGS_ES_HOST"/_settings?pretty" -d \
-'{
-  "index.translog.durability": "async",
-  "index.blocks.read_only_allow_delete": null
 }'
 
 # vim: set ts=4 sw=4 et ai:

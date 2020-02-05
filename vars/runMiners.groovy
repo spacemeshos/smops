45490d8969ce3a299d7a2de645c37f206dc86c11
ilans <buldov@gmail.com>
2020-02-05 17:57:32 +0200

    ...

--- vars/runMiners.groovy
+++ vars/runMiners.groovy
@@ -118,21 +118,18 @@ def call(String aws_region) {
         steps {
           script {
             p = poet_ips.size()
-            stages = [:]
-            worker_ports.eachWithIndex({port, i ->
+            def stepsForParallel = worker_ports.eachWithIndex({port, i ->
               i_str = String.format("%04d", i)
-              stages[i_str] = {
-                echo "i_str: $i_str, port: $port"
-                startMinerNode aws_region: aws_region, pool_id: pool_id, node_id: "${run_id}-node-${port}", \
-                              miner_image: params.MINER_IMAGE, port: port, \
-                              spacemesh_space: SPACEMESH_SPACE, vol_size: vol_size, \
-                              cpu: params.MINER_CPU, mem: params.MINER_MEM, \
-                              params: extra_params, \
-                              poet_ip: poet_ips[i%p], \
-                              labels: params.LABELS
-              }
+              echo "i_str: $i_str, port: $port"
+              startMinerNode aws_region: aws_region, pool_id: pool_id, node_id: "${run_id}-node-${i_str}", \
+                            miner_image: params.MINER_IMAGE, port: port, \
+                            spacemesh_space: SPACEMESH_SPACE, vol_size: vol_size, \
+                            cpu: params.MINER_CPU, mem: params.MINER_MEM, \
+                            params: extra_params, \
+                            poet_ip: poet_ips[i%p], \
+                            labels: params.LABELS
             })
-            parallel stages
+            parallel stepsForParallel
           }
         }
       }
